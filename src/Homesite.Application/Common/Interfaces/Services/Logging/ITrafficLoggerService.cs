06c000b492c7d499dad0152f986db64a5ce1dd9f
Rick Flagg <Richard.Lee.Flagg@outlook.com>
2022-04-06 19:02:51 -0400

    Starting implementation of Traffic Logging

---
+++ src/Homesite.Application/Common/Interfaces/Services/Logging/ITrafficLoggerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homesite.Application.Common.Interfaces.Services.Logging
+{
+    internal interface ITrafficLoggerService
+    {
+    }
+}
