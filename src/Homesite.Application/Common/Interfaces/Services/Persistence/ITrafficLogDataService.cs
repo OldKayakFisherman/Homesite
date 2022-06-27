084f798f41ea54ee1b32048267dc6841a919105c
Rick Flagg <Richard.Lee.Flagg@outlook.com>
2022-06-27 18:15:59 -0400

    Fleshing out traffic log data service

---
+++ src/Homesite.Application/Common/Interfaces/Services/Persistence/ITrafficLogDataService.cs
@@ -0,0 +1,16 @@
+﻿using Homesite.Application.Common.Interfaces.Services.Persistence.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homesite.Application.Common.Interfaces.Services.Persistence
+{
+    public interface ITrafficLogDataService
+    {
+        ITrafficLogDataResult GetCurrentTrafficLogs();
+        ITrafficLogDataResult GetTrafficLogsByIP(string ip);
+        ITrafficLogDataResult GetTrafficLogsByDate(DateTime startDate, DateTime endDate);
+    }
+}
