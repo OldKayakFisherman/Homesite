9bde7602c08c5fbbd3792484a2a95c04b9e01c71
Rick Flagg <Richard.Lee.Flagg@outlook.com>
2022-06-28 19:26:08 -0400

    Fleshing out traffic log persistence implementation

---
+++ src/Homesite.Infrastructure/Services/Persistence/TrafficLogDataService.cs
@@ -0,0 +1,28 @@
+﻿using Homesite.Application.Common.Interfaces.Services.Persistence;
+using Homesite.Application.Common.Interfaces.Services.Persistence.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homesite.Infrastructure.Services.Persistence
+{
+    public class TrafficLogDataService : ITrafficLogDataService
+    {
+        public ITrafficLogDataResult GetCurrentTrafficLogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrafficLogDataResult GetTrafficLogsByDate(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrafficLogDataResult GetTrafficLogsByIP(string ip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Homesite.Infrastructure/Services/Responses/Persistence/TrafficLogDataResult.cs
@@ -0,0 +1,16 @@
+﻿using Homesite.Application.Common.Interfaces.Services.Persistence.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homesite.Infrastructure.Services.Responses.Persistence
+{
+    public class TrafficLogDataResult : ITrafficLogDataResult
+    {
+        public IList<ITrafficDataRecord> Records { get; set; } = new List<ITrafficDataRecord>();
+
+        public ITrafficDataRecord? SingleResult { get; set; }
+    }
+}
