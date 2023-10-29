6c524af01efb822a4842835570a17922d19a8268
Richard Flagg <richard.lee.flagg@outlook.com>
2023-10-29 13:11:58 -0400

    removed C# source

--- src/Homesite.Application/Common/Interfaces/Services/Persistence/IProjectDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Homesite.Application.Common.Interfaces.Services.Persistence.Responses;
-
-namespace Homesite.Application.Common.Interfaces.Services.Persistence;
-
-public interface IProjectDataService
-{
-    Task<IProjectDataResult> All(CancellationToken token);
-    Task DeleteProjects(IList<int> ids, CancellationToken token);
-}
