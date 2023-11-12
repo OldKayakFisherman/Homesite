482fbd0376144cef1061a0673fc8f807f1e8eff0
Richard Flagg <richard.lee.flagg@outlook.com>
2023-11-12 11:17:51 -0500

    Added in flask project

--- main.py
+++ main.py
@@ -1,12 +1,75 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import datetime
+from tinydb import TinyDB, Query
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('.env')
 
 app = Flask(__name__)
+app.secret_key = config['GENERAL']['SECRET.KEY']
+
 
 @app.context_processor
 def inject_copyright_year():
     return {'copyright_year': datetime.now().strftime('%Y')}
 
+@app.context_processor
+def inject_show_contact():
+
+    result = {'show_contact': False}
+
+    if 'show_contact' in session:
+        result = {'show_contact': session['show_contact']}
+
+    session['show_contact'] = False
+
+    return result
+
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.post("/contact")
+def contact():
+
+    # Check for the nonce, if there is then this was most likely submitted by an auto process
+
+    if 'chkNonce' not in request.form:
+        if request.form['txtContactEmail'] and request.form['txtReason']:
+            write_contact_request(str(request.form['txtContactEmail']),
+                                  str(request.form['txtReason']))
+
+            session['show_contact'] = True
+
+    return redirect(url_for('index'))
+
+
+@app.get("/contact_submissions")
+def contact_submissions():
+    db = TinyDB('db.json')
+    token = request.args.get('token', '')
+    stored_token = config['GENERAL']['SECRET.KEY']
+
+    if token == stored_token:
+        return json.dumps(db.all())
+
+
+def write_contact_request(email:str, reason:str):
+
+    db = TinyDB('db.json')
+
+    # Check to see if there is a previous contact
+    Contact = Query()
+
+    result = db.search(Contact.email == email)
+
+    print(result)
+
+    if not result:
+        # User hasn't contacted us previously
+        db.insert({'email': email, 'reason': reason})
+
+
+
--- static/js/IndexController.js
+++ static/js/IndexController.js
@@ -12,6 +12,7 @@
     let navClients = document.getElementById('navClients');
     let navContact = document.getElementById('navContact');
 
+    let hdnContact = document.getElementById('hdnContactSubmitted');
 
     hideAll();
     show(home);
@@ -20,34 +21,41 @@
         evt.preventDefault();
         hideAll();
         show(home);
+        disableAllActiveNavs();
+        navHome.classList.add("active");
     });
 
     navAbout.addEventListener("click", (evt) => {
         evt.preventDefault();
         hideAll();
         show(about);
+        disableAllActiveNavs();
+        navAbout.classList.add("active");
     });
 
     navSkills.addEventListener("click", (evt) => {
         evt.preventDefault();
         hideAll();
         show(skills);
+        disableAllActiveNavs();
+        navSkills.classList.add("active");
     });
 
     navClients.addEventListener("click", (evt) => {
         evt.preventDefault();
         hideAll();
         show(clients);
+        disableAllActiveNavs();
+        navClients.classList.add("active");
     });
 
     navContact.addEventListener("click", (evt) => {
         evt.preventDefault();
         hideAll();
         show(contact);
+        disableAllActiveNavs();
+        navContact.classList.add("active");
     });
-
-    //Handle Scroll Events
-
 
     function hideAll(){
         home.setAttribute('hidden', null);
@@ -63,10 +71,25 @@
         contact.classList.remove('active')
     }
 
-    function show(element){
-        element.removeAttribute('hidden');
-        element.classList.add('active');
+    function disableAllActiveNavs()
+    {
+        navHome.classList.remove('active');
+        navAbout.classList.remove('active');
+        navSkills.classList.remove('active');
+        navClients.classList.remove('active');
+        navContact.classList.remove('active');
     }
 
+    function show(element){
+        element.removeAttribute('hidden');
+    }
+
+    if (hdnContact.value === "True"){
+        console.log("Showing Modal");
+        let elem = document.getElementById('mdlContactRequest')
+        let modal = new bootstrap.Modal(elem)
+        modal.show()
+    }
 
 })();
+
